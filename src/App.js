b60ad108a60f978ca80bd8464a1eff941f656494
Andriikryl <andreyk557@gmail.com>
2023-04-23 01:24:49 +0300

    feat[Editor - component]

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+import { useState } from "react";
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
+  const [openedEditor, setOpenedEditor] = useState("html");
+
+  const onTabClick = (editorName) => {
+    setOpenedEditor(editorName);
+  };
+
   return (
     <div className="App">
-      <h1>sfa</h1>
+      <h1>Welcome to the my editor</h1>
+      <div>
+        <Button
+          title="HTML"
+          onClick={() => {
+            onTabClick("html");
+          }}
+        />
+        <Button
+          title="CSS"
+          onClick={() => {
+            onTabClick("css");
+          }}
+        />
+        <Button
+          title="JavaScript"
+          onClick={() => {
+            onTabClick("js");
+          }}
+        />
+      </div>
+
+      <div className="editor-container">
+        {openedEditor === "html" ? (
+          <p>The html editor is open</p>
+        ) : openedEditor === "css" ? (
+          <p>The css editor is open</p>
+        ) : (
+          <p>The JavaScript editor is open</p>
+        )}
+      </div>
     </div>
   );
 }
