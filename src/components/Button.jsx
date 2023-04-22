2f2e8596c353113c81d87b8cbc2cdd78c0fca8f5
Andriikryl <andreyk557@gmail.com>
2023-04-22 23:58:41 +0300

    feat[button-component]

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+
+const Button = ({title, onClick}) => {
+    return (
+        <div>
+            <button onClick={onClick} >
+                {title}
+            </button>
+        </div>
+    )
+}
+
+export default Button
